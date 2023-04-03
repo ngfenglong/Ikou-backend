436720b113b5468eaf0939d050783a5b8917d27b
fenglong_ng <fenglong1502@hotmail.com>
2023-04-03 09:08:12 +0800

    Added response writer  and login helper method

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -2,10 +2,14 @@ package helper
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+func WriteJSONResponse(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
 	out, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
@@ -22,4 +26,76 @@ func WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...h
 	w.Write(out)
 
 	return nil
+}
+
+func ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1048576
+
+	// Set a non-error checker to prevent malicious user from flooding the request
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only have single JSON value")
+	}
+
+	return nil
+}
+
+func BadRequest(w http.ResponseWriter, r *http.Request, err error) error {
+	var payload struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+
+	payload.Error = true
+	payload.Message = err.Error()
+
+	out, err := json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		return nil
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
+
+	return nil
+}
+
+func invalidCredential(w http.ResponseWriter) error {
+	var payload struct {
+		Error   bool   `json:"error"`
+		Message string `json:"message"`
+	}
+
+	payload.Error = true
+	payload.Message = "Invalid authentication credential"
+
+	err := WriteJSONResponse(w, http.StatusUnauthorized, payload)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func passwordMatches(hash, password string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default: 
+			return false, err
+		}
+	}
+
+	return true, nil
 }
